51999d0ea21bae079a72472a3bfbc96b4228eb38
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2023-03-26 12:39:49 -0500

    feat: pages first part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
-    path: 'dashboard',
+    path: '',
     loadChildren: () => import('./ggi/ggi.module').then(m => m.GgiModule)
   },
   {
--- src/app/ggi/pages/pages.component.ts
+++ src/app/ggi/pages/pages.component.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: [
+    `
+    mat-sidenav-container {
+      height: calc(205vh - 115vh);
+    }
+
+    mat-sidenav {
+      width: 200px;
+      background-color: red;
+    }
+    `
   ]
 })
 export class PagesComponent {
-
+  opened = false;
 }
